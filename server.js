1b970a51393c8ac4947c0e331c149643b0ac5b36
wladij <joew.candelario@gmail.com>
2025-11-03 14:43:04 -0500

    commit finished

--- server.js
+++ server.js
@@ -5,15 +5,29 @@
 /* ***********************
  * Require Statements
  *************************/
+const expressLayouts = require("express-ejs-layouts")
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+// Index route
+app.get("/", function (rep, res) {
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
